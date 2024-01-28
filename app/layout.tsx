a12ea50e145d7315429106bd9a1011861e63bc6f
tanjul sarathe <you@example.com>
2024-01-29 00:21:55 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+import Header from '@/components/shared/Header'
+import Footer from '@/components/shared/Footer'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'AlgoEvent',
+  description: 'AlgoEvent is a platform for  Technical event management.',
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
